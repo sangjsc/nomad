b7d49ee01d5906e0e9e959233f66a2800925443f
sangjsc <nwing920@naver.com>
2025-07-15 09:17:36 +0900

    홈페이지 내용 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,9 @@ export const metadata: Metadata = {
   description: "경기도 이천, 광주, 여주, 용인 전지역 빠르게 찾아가는 노마드출장마사지! 최고의 실력을 갖춘 관리사가 편안한 공간에서 피로를 풀어드립니다. 지금 바로 예약하세요.",
   keywords: "이천출장마사지, 광주출장마사지, 여주출장마사지, 용인출장마사지, 홈타이, 방문마사지, 아로마마사지, 출장마사지, 방문마사지",
   icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon.ico',
-    apple: '/icon-192.png',
+    icon: '/favicon.svg',
+    shortcut: '/favicon.svg',
+    apple: '/icon-192.svg',
   },
   manifest: '/manifest.json',
 }
