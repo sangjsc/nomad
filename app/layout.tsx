7312d42651064a01af08276d7b5d028845c663cc
sangjsc <nwing920@naver.com>
2025-07-16 07:28:27 +0900

    홈페이지 내용 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,13 @@ import MobileBottomNav from '@/components/MobileBottomNav'
 import ScrollToTop from '@/components/ScrollToTop'
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://nomadthai.kr'),
   title: "노마드출장마사지 | 이천, 광주, 여주, 용인 빠르게 찾아가는 프리미엄 마사지",
   description: "경기도 이천, 광주, 여주, 용인 전지역 빠르게 찾아가는 노마드출장마사지! 최고의 실력을 갖춘 관리사가 편안한 공간에서 피로를 풀어드립니다. 지금 바로 예약하세요.",
   keywords: "이천출장마사지, 광주출장마사지, 여주출장마사지, 용인출장마사지, 홈타이, 방문마사지, 아로마마사지, 출장마사지, 방문마사지",
   manifest: '/manifest.json',
-  viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
   icons: {
-    icon: '/favicon.svg',
-    apple: '/apple-icon.png'
+    icon: '/favicon.svg'
   },
   openGraph: {
     title: "노마드출장마사지 | 이천, 광주, 여주, 용인 프리미엄 마사지",
@@ -28,6 +27,13 @@ export const metadata: Metadata = {
       follow: true,
     },
   }
+}
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 }
 
 export default function RootLayout({
